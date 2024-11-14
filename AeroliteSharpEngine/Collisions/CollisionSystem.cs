d9945f23688d536b825d0e8f28bf11a1bcbe773a
Brandon Berisford <brandonberisford@gmail.com>
2024-11-14 09:40:46 -0600

    Adding regions to collision system file

--- AeroliteSharpEngine/Collisions/CollisionSystem.cs
+++ AeroliteSharpEngine/Collisions/CollisionSystem.cs
@@ -11,97 +11,108 @@ namespace AeroliteSharpEngine.Collisions
     /// <summary>
     /// Main collision detection system that coordinates broad and narrow phase collision routines.
     /// </summary>
-    public class CollisionSystem : ICollisionSystem
-{
-    private readonly List<CollisionManifold> _collisions = [];
-    private readonly HashSet<(IPhysicsObject2D, IPhysicsObject2D)> _potentialPairs = [];
-    private readonly HashSet<int> _validatedConvexObjectIds = [];
-
-    public CollisionSystem(CollisionSystemConfiguration collisionSystemConfiguration)
+    public class CollisionSystem(CollisionSystemConfiguration collisionSystemConfiguration) : ICollisionSystem
     {
-        Configuration = collisionSystemConfiguration;
-    }
+        #region Fields
 
-    public CollisionSystemConfiguration Configuration { get; }
-    public IEnumerable<CollisionManifold> Collisions => _collisions;
-    public IEnumerable<(IPhysicsObject2D, IPhysicsObject2D)> PotentialPairs => _potentialPairs;
+        private readonly List<CollisionManifold> _collisions = [];
+        private readonly HashSet<(IPhysicsObject2D, IPhysicsObject2D)> _potentialPairs = [];
+        private readonly HashSet<int> _validatedConvexObjectIds = [];
 
-    public void Clear()
-    {
-        // Return all manifolds to the pool
-        foreach (var manifold in _collisions)
-        {
-            CollisionPoolService.Instance.ReturnManifold(manifold);
-        }
+        #endregion
+
+        #region Properties
+    
+        public CollisionSystemConfiguration Configuration { get; } = collisionSystemConfiguration;
+        public IEnumerable<CollisionManifold> Collisions => _collisions;
+        public IEnumerable<(IPhysicsObject2D, IPhysicsObject2D)> PotentialPairs => _potentialPairs;
         
-        _validatedConvexObjectIds.Clear();
-        _collisions.Clear();
-        _potentialPairs.Clear();
-    }
-
-    public void HandleCollisions(IReadOnlyList<IPhysicsObject2D>? objects)
-    {
-        if (objects == null || objects.Count == 0) return;
-        if (Configuration.ValidateConvexShapes)
-        {
-            ValidateNewShapes(objects);
-        }
-
-        // Return existing manifolds to the pool
-        foreach (var manifold in _collisions)
-        {
-            CollisionPoolService.Instance.ReturnManifold(manifold);
-        }
+        #endregion
         
-        _collisions.Clear();
-        _potentialPairs.Clear();
-        
-        // First broad phase: Spatial partitioning
-        Configuration.BroadPhase.Update(objects);
-        var potentialPairs = Configuration.BroadPhase.FindPotentialCollisions().ToList();
-
-        if (potentialPairs.Count == 0) return;
-        
-        foreach (var (objA, objB) in potentialPairs)
+        #region Public Methods
+    
+        public void Clear()
         {
-            _potentialPairs.Add((objA, objB));
-            
-            var manifold = Configuration.NarrowPhase.TestCollision(objA, objB);
-            if (manifold.HasCollision)
-            {
-                _collisions.Add(manifold);
-            }
-            else
+            // Return all manifolds to the pool
+            foreach (var manifold in _collisions)
             {
                 CollisionPoolService.Instance.ReturnManifold(manifold);
             }
+            
+            _validatedConvexObjectIds.Clear();
+            _collisions.Clear();
+            _potentialPairs.Clear();
         }
-        
-        // Resolve collisions
-        if(_collisions.Count > 0)
+    
+        public void HandleCollisions(IReadOnlyList<IPhysicsObject2D>? objects)
         {
-            Configuration.CollisionResolver.ResolveCollisions(_collisions);
-        }
-    }
-
-    private void ValidateNewShapes(IReadOnlyList<IPhysicsObject2D> objects)
-    {
-        if (Configuration.Type != CollisionSystemType.ConvexOnly) return;
-        foreach (var obj in objects)
-        {
-            // Only validate objects we haven't seen before
-            if (obj is not AeroBody2D body || _validatedConvexObjectIds.Contains(body.Id)) continue;
-            if (body.Shape is not IConvexShape)
+            if (objects == null || objects.Count == 0) return;
+            if (Configuration.ValidateConvexShapes)
             {
-                if (body.Shape is AeroPolygon polygon && !polygon.IsConvex) 
+                ValidateNewShapes(objects);
+            }
+    
+            // Return existing manifolds to the pool
+            foreach (var manifold in _collisions)
+            {
+                CollisionPoolService.Instance.ReturnManifold(manifold);
+            }
+            
+            _collisions.Clear();
+            _potentialPairs.Clear();
+            
+            // First broad phase: Spatial partitioning
+            Configuration.BroadPhase.Update(objects);
+            var potentialPairs = Configuration.BroadPhase.FindPotentialCollisions().ToList();
+    
+            if (potentialPairs.Count == 0) return;
+            
+            foreach (var (objA, objB) in potentialPairs)
+            {
+                _potentialPairs.Add((objA, objB));
+                
+                var manifold = Configuration.NarrowPhase.TestCollision(objA, objB);
+                if (manifold.HasCollision)
                 {
-                    throw new InvalidOperationException(
-                        $"Object {obj} uses non-convex shape " +
-                        "but the collision system is configured for convex-only shapes.");
+                    _collisions.Add(manifold);
+                }
+                else
+                {
+                    CollisionPoolService.Instance.ReturnManifold(manifold);
                 }
             }
-            _validatedConvexObjectIds.Add(body.Id);
+            
+            // Resolve collisions
+            if(_collisions.Count > 0)
+            {
+                Configuration.CollisionResolver.ResolveCollisions(_collisions);
+            }
         }
+        
+        #endregion
+        
+        #region Private Methods
+    
+        private void ValidateNewShapes(IReadOnlyList<IPhysicsObject2D> objects)
+        {
+            if (Configuration.Type != CollisionSystemType.ConvexOnly) return;
+            foreach (var obj in objects)
+            {
+                // Only validate objects we haven't seen before
+                if (obj is not AeroBody2D body || _validatedConvexObjectIds.Contains(body.Id)) continue;
+                if (body.Shape is not IConvexShape)
+                {
+                    if (body.Shape is AeroPolygon polygon && !polygon.IsConvex) 
+                    {
+                        throw new InvalidOperationException(
+                            $"Object {obj} uses non-convex shape " +
+                            "but the collision system is configured for convex-only shapes.");
+                    }
+                }
+                _validatedConvexObjectIds.Add(body.Id);
+            }
+        }
+        
+        #endregion
     }
-}
 }
